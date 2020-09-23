b4aacbf52692a8bed12875ff98339fd0aedd4a7c
Randy Widell <randy@slakr.org>
2020-09-23 10:56:35 -0800

    Update CMakeLists.txt to handle all libraries.

---
+++ cmake/FindJansson.cmake
@@ -0,0 +1,5 @@
+find_library(JANSSON_LIBRARIES NAMES jansson)
+find_path(JANSSON_INCLUDE_DIRS NAMES jansson.h)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(jansson DEFAULT_MSG JANSSON_LIBRARIES JANSSON_INCLUDE_DIRS)
