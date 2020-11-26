bf60aa4b7f9269e82acfa20acf3400c6a5b17867
Randy Widell <randy@slakr.org>
2020-11-26 14:03:57 -0800

    Initial translation of LED driver.

--- docmake_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export CC=gcc-8
-export CXX=g++-8
-cmake -DCMAKE_INSTALL_PREFIX=$(realpath ..) -DETC_PREFIX="./" -DCMAKE_BUILD_TYPE=Debug ..
--- docmake_release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export CC=gcc-8
-export CXX=g++-8
-cmake -DCMAKE_INSTALL_PREFIX="/usr/local/piwx" -DCMAKE_BUILD_TYPE=Release ..
-
