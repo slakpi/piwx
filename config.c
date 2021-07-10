8c30f231fc07ad3a1c1339ae91fc0f6a4246abc8
Randy Widell <randy@slakr.org>
2021-07-10 09:29:14 -0700

    Cleaning up the config code.

--- config.c
+++ config.c
@@ -1,38 +1,46 @@
-#include <string.h>
 #include <config.h>
+#include <string.h>
 
-typedef void* yyscan_t;
+typedef void *yyscan_t;
 
+// clang-format off
+// These includes must be in this order.
 #include "config_helpers.h"
 #include "conf_file.h"
 #include "conf_file.parser.h"
+// clang-format on
 
 #define YYSTYPE CONF_STYPE
 
 #include "conf_file.lexer.h"
 
-PiwxConfig* getPiwxConfig()
-{
+PiwxConfig *getPiwxConfig() {
   FILE *cfgFile;
   yyscan_t scanner;
-  PiwxConfig *cfg = (PiwxConfig*)malloc(sizeof(PiwxConfig));
+  PiwxConfig *cfg = (PiwxConfig *)malloc(sizeof(PiwxConfig));
+
+  if (!cfg) {
+    return NULL;
+  }
 
   memset(cfg, 0, sizeof(PiwxConfig));
-  cfg->installPrefix = strdup(INSTALL_PREFIX);
-  cfg->imageResources = strdup(IMAGE_RESOURCES);
-  cfg->fontResources = strdup(FONT_RESOURCES);
-  cfg->configFile = strdup(CONFIG_FILE);
-  cfg->cycleTime = 60;
-  cfg->highWindSpeed = 25;
-  cfg->highWindBlink = 0;
-  cfg->ledBrightness = 32;
+  cfg->installPrefix      = strdup(INSTALL_PREFIX);
+  cfg->imageResources     = strdup(IMAGE_RESOURCES);
+  cfg->fontResources      = strdup(FONT_RESOURCES);
+  cfg->configFile         = strdup(CONFIG_FILE);
+  cfg->cycleTime          = 60;
+  cfg->highWindSpeed      = 25;
+  cfg->highWindBlink      = 0;
+  cfg->ledBrightness      = 32;
   cfg->ledNightBrightness = 32;
-  cfg->ledDataPin = 18;
-  cfg->ledDMAChannel = 10;
+  cfg->ledDataPin         = 18;
+  cfg->ledDMAChannel      = 10;
 
   cfgFile = fopen(CONFIG_FILE, "r");
-  if (!cfgFile)
+
+  if (!cfgFile) {
     return cfg;
+  }
 
   conf_lex_init(&scanner);
   conf_set_in(cfgFile, scanner);
@@ -44,47 +52,83 @@ PiwxConfig* getPiwxConfig()
   return cfg;
 }
 
-void freePiwxConfig(PiwxConfig *_cfg)
-{
+void freePiwxConfig(PiwxConfig *_cfg) {
   int i;
 
-  if (_cfg->installPrefix)
-    free(_cfg->installPrefix);
-  if (_cfg->imageResources)
-    free(_cfg->imageResources);
-  if (_cfg->fontResources)
-    free(_cfg->fontResources);
-  if (_cfg->configFile)
-    free(_cfg->configFile);
-  if (_cfg->stationQuery)
-    free(_cfg->stationQuery);
-  if (_cfg->nearestAirport)
-    free(_cfg->nearestAirport);
+  if (!_cfg) {
+    return;
+  }
 
-  for (i = 0; i < MAX_LEDS; ++i)
-  {
-    if (_cfg->ledAssignments[i])
+  if (_cfg->installPrefix) {
+    free(_cfg->installPrefix);
+  }
+
+  if (_cfg->imageResources) {
+    free(_cfg->imageResources);
+  }
+
+  if (_cfg->fontResources) {
+    free(_cfg->fontResources);
+  }
+
+  if (_cfg->configFile) {
+    free(_cfg->configFile);
+  }
+
+  if (_cfg->stationQuery) {
+    free(_cfg->stationQuery);
+  }
+
+  if (_cfg->nearestAirport) {
+    free(_cfg->nearestAirport);
+  }
+
+  for (i = 0; i < MAX_LEDS; ++i) {
+    if (_cfg->ledAssignments[i]) {
       free(_cfg->ledAssignments[i]);
+    }
   }
 
   free(_cfg);
 }
 
-static char* appendFileToPath(const char *_prefix, const char *_file, char *_path, size_t _len)
-{
+/**
+ * @brief   Append a file name to a path prefix.
+ * @details A POSIX-only function to append a trailing backslash to the prefix,
+ *          if necessary, followed by the specified file name.
+ * @param[in] _prefix Null-terminated path prefix.
+ * @param[in] _file   Null-terminated file name.
+ * @param[in] _path   Output buffer.
+ * @param[in] _len    Length of the output buffer.
+ * @returns The buffer pointer or NULL if the output buffer is too small.
+ */
+static char *appendFileToPath(const char *_prefix, const char *_file,
+                              char *_path, size_t _len) {
   size_t pl = strlen(_prefix), fl = strlen(_file);
 
-  if (_prefix[pl - 1] != '/')
+  // If the prefix does not already have a trailing backslash, add one to the
+  // length to account for adding the backslash.
+  if (_prefix[pl - 1] != '/') {
     ++pl;
-  if (pl + fl + 1 >= _len)
-    return NULL;
+  }
 
+  // If the prefix length + file length + null terminator is larger than the
+  // buffer, return NULL.
+  if (pl + fl + 1 >= _len) {
+    return NULL;
+  }
+
+  // Make sure the prefix copies to the beginning of the output buffer.
   _path[0] = 0;
 
   strcat(_path, _prefix);
 
-  if (_path[pl - 1] != '/')
-  {
+  // If the first check above did not find a trailing backslash, it increased
+  // the length by one. `pl - 1` would now point to the null terminator. If it
+  // did find a trailing backslash, then `pl - 1` still points to the backslash.
+  // Overwrite the null terminator with the backslash, and set the new null
+  // terminator so the file name copy occurs after the backslash.
+  if (_path[pl - 1] != '/') {
     _path[pl - 1] = '/';
     _path[pl] = 0;
   }
@@ -94,12 +138,10 @@ static char* appendFileToPath(const char *_prefix, const char *_file, char *_pat
   return _path;
 }
 
-char* getPathForBitmap(const char *_file, char *_path, size_t _len)
-{
+char *getPathForBitmap(const char *_file, char *_path, size_t _len) {
   return appendFileToPath(IMAGE_RESOURCES, _file, _path, _len);
 }
 
-char* getPathForFont(const char *_file, char *_path, size_t _len)
-{
+char *getPathForFont(const char *_file, char *_path, size_t _len) {
   return appendFileToPath(FONT_RESOURCES, _file, _path, _len);
 }
--- config_helpers.h
+++ config_helpers.h
@@ -3,27 +3,66 @@
 
 #define MAX_LEDS 50
 
-typedef struct __PiwxConfig
-{
-  char *installPrefix;
-  char *imageResources;
-  char *fontResources;
-  char *configFile;
-  char *stationQuery;
-  char *nearestAirport;
-  int cycleTime;
-  int highWindSpeed;
-  int highWindBlink;
-  char* ledAssignments[MAX_LEDS];
-  int ledBrightness;
-  int ledNightBrightness;
-  int ledDataPin;
-  int ledDMAChannel;
+/**
+ * @struct  PiwxConfig
+ * @brief   The configuration structure.
+ * @details The @a installPrefix, @a imageResources, @a fontResoures, and
+ *          @a configFile members are informational only. The remaining members
+ *          contain the values read from @a CONFIG_FILE or the defaults.
+ */
+typedef struct {
+  char *installPrefix;                 /* Compile-time install prefix.        */
+  char *imageResources;                /* Compile-time image resources path.  */
+  char *fontResources;                 /* Compile-time font resources path.   */
+  char *configFile;                    /* Compile-time config file path.      */ 
+  char *stationQuery;                  /* List of weather stations to query.  */
+  char *nearestAirport;                /* Nearest airport for LED dimming.    */
+  int cycleTime;                       /* Airport display cycle time in sec.  */
+  int highWindSpeed;                   /* High wind threshold in knts.        */
+  int highWindBlink;                   /* High wind blink rate in sec.        */
+  char *ledAssignments[MAX_LEDS];      /* Airport LED assignments.            */
+  int ledBrightness;                   /* Day LED brightness, 0-255           */
+  int ledNightBrightness;              /* Night LED brightness, 0-255         */
+  int ledDataPin;                      /* LED Rpi data pin.                   */
+  int ledDMAChannel;                   /* LED Rpi DMA channel                 */
 } PiwxConfig;
 
-PiwxConfig* getPiwxConfig();
+/**
+ * @brief   Load the piwx configuration.
+ * @details Loads the piwx configuration from @a CONFIG_FILE or initializes the
+ *          configuration structure with the piwx defaults if the file does not
+ *          exist or is invalid.
+ * @returns An initialized configuration structure or NULL if unable to allocate
+ *          memory for the structure.
+ */
+PiwxConfig *getPiwxConfig();
+
+/**
+ * @brief Free an allocated configuration struct.
+ * @param[in] _cfg The configuration struct to free.
+ */
 void freePiwxConfig(PiwxConfig *_cfg);
-char* getPathForBitmap(const char *_file, char *_path, size_t _len);
-char* getPathForFont(const char *_file, char *_path, size_t _len);
+
+/**
+ * @brief   Load the fully-qualified path to the specified bitmap file.
+ * @details Appends @a _file to the @a IMAGE_RESOURCES path using @a _path as
+ *          an output buffer.
+ * @param[in] _file The bitmap file to append.
+ * @param[in] _path The buffer to hold the fully-qualified path.
+ * @param[in] _len  The length of @a path.
+ * @returns The buffer pointer or NULL if the output buffer is too small.
+ */
+char *getPathForBitmap(const char *_file, char *_path, size_t _len);
+
+/**
+ * @brief   Load the fully-qualified path to the specified bitmap file.
+ * @details Appends @a _file to the @a FONT_RESOURCES path using @a _path as an
+ *          output buffer.
+ * @param[in] _file The font file to append.
+ * @param[in] _path The buffer to hold the fully-qualified path.
+ * @param[in] _len  The length of @a path.
+ * @returns The buffer pointer or NULL if the output buffer is too small.
+ */
+char *getPathForFont(const char *_file, char *_path, size_t _len);
 
 #endif
