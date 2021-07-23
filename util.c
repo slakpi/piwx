fd720d7e37b62c2ccdde77760083014fb303fc1f
Randy Widell <randy@slakr.org>
2021-07-22 19:42:39 -0700

    Cleaned up utility code.

--- util.c
+++ util.c
@@ -1,12 +1,9 @@
 #include "util.h"
 
-int min(int _a, int _b)
-{
+int min(int _a, int _b) {
   return (_a < _b ? _a : _b);
 }
 
-int max(int _a, int _b)
-{
+int max(int _a, int _b) {
   return (_a > _b ? _a : _b);
 }
-
