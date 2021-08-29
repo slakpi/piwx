8537d36759c8b675afb764c9b51e97409ca9bc04
Randy Widell <randy@slakr.org>
2021-08-29 08:18:33 -0700

    Added find package scripts for all libraries.

--- cmake/FindWiringPi.cmake
+++ cmake/FindWiringPi.cmake
@@ -2,4 +2,4 @@ find_library(WIRINGPI_LIBRARIES NAMES wiringPi)
 find_path(WIRINGPI_INCLUDE_DIRS NAMES wiringPi.h)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(wiringPi DEFAULT_MSG WIRINGPI_LIBRARIES WIRINGPI_INCLUDE_DIRS)
+find_package_handle_standard_args(WIRINGPI DEFAULT_MSG WIRINGPI_LIBRARIES WIRINGPI_INCLUDE_DIRS)
