9638d03a27c57c1582b0dd6e4b3dae3cfe21c532
Randy Widell <randy@slakr.org>
2022-03-05 10:09:36 -0800

    Tidy'ing and formatting util sources.

--- util.c
+++ util.c
@@ -8,16 +8,16 @@ int min(int a, int b) { return (a < b ? a : b); }
 
 int max(int a, int b) { return (a > b ? a : b); }
 
-size_t strncpy_safe(char * dst, const char * src, size_t maxlen) {
+size_t strncpy_safe(char *dst, const char *src, size_t maxlen) {
   // Based on Apple's implementation of @a strlcpy in XNU.
 
   const size_t srclen = strlen(src);
 
   if (srclen + 1 < maxlen) {
-    memcpy(dst, src, srclen + 1);
+    memcpy(dst, src, srclen + 1); // NOLINT -- Size checked.
   } else if (maxlen != 0) {
-    memcpy(dst, src, maxlen - 1);
-    dst[maxlen-1] = '\0';
+    memcpy(dst, src, maxlen - 1); // NOLINT -- Size checked.
+    dst[maxlen - 1] = '\0';
   }
 
   return srclen;
