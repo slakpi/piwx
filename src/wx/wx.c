a716373b226e616e3cb160547fa481e380162171
Randy Widell <randy@slakr.org>
2023-08-14 19:16:48 -0700

    Weather source cleanup.

--- src/wx/wx.c
+++ src/wx/wx.c
@@ -24,6 +24,308 @@ typedef void *yyscan_t;
 #define MAX_DATETIME_LEN 20
 
 /**
+ * @enum  Tag
+ * @brief METAR XML tag ID.
+ */
+// clang-format off
+typedef enum {
+  tagInvalid = -1,
+
+  tagResponse = 1,
+
+  tagData,
+
+  tagMETAR,
+
+  tagRawText,  tagStationId, tagObsTime,   tagLat,      tagLon, tagTemp,
+  tagDewpoint, tagWindDir,   tagWindSpeed, tagWindGust, tagVis, tagAlt,
+  tagCategory, tagWxString,  tagSkyCond,   tagVertVis,
+
+  tagSkyCover, tagCloudBase,
+
+  tagSCT,      tagFEW,       tagBKN,       tagOVC,      tagOVX, tagCLR,
+  tagSKC,      tagCAVOK,
+
+  tagVFR,      tagMVFR,      tagIFR,       tagLIFR,
+
+  tagFirst = tagResponse,
+  tagLast = tagLIFR
+} Tag;
+// clang-format on
+
+/**
+ * @enum Intensity
+ * @brief Weather intensity value.
+ */
+typedef enum { intensityInvalid, intensityLight, intensityModerate, intensityHeavy } Intensity;
+
+/**
+ * @struct METARCallbackData
+ * @brief  User data structure for parsing METAR XML.
+ */
+typedef struct {
+  xmlParserCtxtPtr ctxt;
+} METARCallbackData;
+
+// clang-format off
+static const Tag gTags[] = {
+  tagResponse,
+
+  tagData,
+
+  tagMETAR,
+
+  tagRawText,  tagStationId, tagObsTime,   tagLat,      tagLon, tagTemp,
+  tagDewpoint, tagWindDir,   tagWindSpeed, tagWindGust, tagVis, tagAlt,
+  tagCategory, tagWxString,  tagSkyCond,   tagVertVis,
+
+  tagSkyCover, tagCloudBase,
+
+  tagSCT,      tagFEW,       tagBKN,       tagOVC,      tagOVX, tagCLR,
+  tagSKC,      tagCAVOK,
+
+  tagVFR,      tagMVFR,      tagIFR,       tagLIFR,
+};
+// clang-format on
+
+// clang-format off
+static const char *gTagNames[] = {
+  "response",
+
+  "data",
+
+  "METAR",
+
+  "raw_text",
+  "station_id",
+  "observation_time",
+  "latitude",
+  "longitude",
+  "temp_c",
+  "dewpoint_c",
+  "wind_dir_degrees",
+  "wind_speed_kt",
+  "wind_gust_kt",
+  "visibility_statute_mi",
+  "altim_in_hg",
+  "flight_category",
+  "wx_string",
+  "sky_condition",
+  "vert_vis_ft",
+
+  "sky_cover",
+  "cloud_base_ft_agl",
+
+  "SCT",
+  "FEW",
+  "BKN",
+  "OVC",
+  "OVX",
+  "CLR",
+  "SKC",
+  "CAVOK",
+
+  "VFR",
+  "MVFR",
+  "IFR",
+  "LIFR",
+
+  NULL
+};
+// clang-format on
+
+static void addCloudLayer(xmlNodePtr node, WxStation *station, xmlHashTablePtr hash);
+
+static void classifyDominantWeather(WxStation *station);
+
+static xmlNodePtr getChildTag(xmlNodePtr children, Tag tag, xmlHashTablePtr hash);
+
+static CloudCover getLayerCloudCover(xmlAttr *attr, xmlHashTablePtr hash);
+
+static FlightCategory getStationFlightCategory(xmlNodePtr node, xmlHashTablePtr hash);
+
+static Tag getTag(xmlHashTablePtr hash, const xmlChar *tag);
+
+static void hashDealloc(void *payload, xmlChar *name);
+
+static void initHash(xmlHashTablePtr hash);
+
+static size_t metarCallback(char *ptr, size_t size, size_t nmemb, void *userdata);
+
+static bool parseUTCDateTime(const char *str, struct tm *tm);
+
+static void readStation(xmlNodePtr node, xmlHashTablePtr hash, WxStation *station);
+
+static char *trimLocalId(const char *id);
+
+void freeStations(WxStation *stations) {
+  WxStation    *p;
+  SkyCondition *s;
+
+  if (!stations) {
+    return;
+  }
+
+  // Break the circular list.
+  stations->prev->next = NULL;
+
+  while (stations) {
+    p = stations;
+
+    stations = stations->next;
+
+    free(p->id);
+    free(p->localId);
+    free(p->raw);
+
+    while (p->layers) {
+      s         = p->layers;
+      p->layers = p->layers->next;
+      free(s);
+    }
+
+    free(p);
+  }
+}
+
+WxStation *queryWx(const char *stations, int *err) {
+  CURL             *curlLib;
+  CURLcode          res;
+  char              url[4096];
+  METARCallbackData data;
+  xmlDocPtr         doc = NULL;
+  xmlNodePtr        p;
+  xmlHashTablePtr   hash = NULL;
+  Tag               tag;
+  WxStation        *start = NULL, *cur, *newStation;
+  int               count, len;
+  bool              ok = false;
+  time_t            now = time(NULL);
+
+  *err = 0;
+
+  // Build the query string to look for reports within the last hour and a half.
+  // It is possible some stations lag more than an hour, but typically not more
+  // than an hour and a half.
+  count = strncpy_safe(url,
+                       "https://aviationweather.gov/adds/dataserver_current/httpparam?"
+                       "dataSource=metars&"
+                       "requestType=retrieve&"
+                       "format=xml&"
+                       "hoursBeforeNow=1.5&"
+                       "mostRecentForEachStation=true&"
+                       "stationString=",
+                       COUNTOF(url));
+
+  assertLog(count < COUNTOF(url), "Base URL is too large.");
+  count = COUNTOF(url) - strlen(url);
+  len   = strlen(stations);
+
+  // TODO: Eventually this should split the station string into multiple queries
+  // if it is too long. For now, just abort.
+  if (len >= count) {
+    return NULL;
+  }
+
+  // NOLINTNEXTLINE -- strncat is sufficient; sizes checked above.
+  strncat(url, stations, count);
+
+  curlLib = curl_easy_init();
+
+  if (!curlLib) {
+    return NULL;
+  }
+
+  data.ctxt = NULL;
+  curl_easy_setopt(curlLib, CURLOPT_URL, url);
+  curl_easy_setopt(curlLib, CURLOPT_WRITEFUNCTION, metarCallback);
+  curl_easy_setopt(curlLib, CURLOPT_WRITEDATA, &data);
+  res = curl_easy_perform(curlLib);
+  curl_easy_cleanup(curlLib);
+
+  if (res != CURLE_OK || !data.ctxt) {
+    *err = res;
+    return NULL;
+  }
+
+  xmlParseChunk(data.ctxt, NULL, 0, 1);
+  doc = data.ctxt->myDoc;
+  xmlFreeParserCtxt(data.ctxt);
+
+  hash = xmlHashCreate(tagLast);
+  initHash(hash);
+
+  // Find the response tag.
+  p = getChildTag(doc->children, tagResponse, hash);
+
+  if (!p) {
+    goto cleanup;
+  }
+
+  // Find the data tag.
+  p = getChildTag(p->children, tagData, hash);
+
+  if (!p) {
+    goto cleanup;
+  }
+
+  // Scan for METAR groups.
+  p = p->children;
+
+  while (p) {
+    tag = getTag(hash, p->name);
+
+    if (tag != tagMETAR) {
+      p = p->next;
+      continue;
+    }
+
+    newStation = (WxStation *)malloc(sizeof(WxStation));
+    memset(newStation, 0, sizeof(WxStation)); // NOLINT -- Size known.
+
+    // Add the station to the circular list.
+    if (!start) {
+      start = cur = newStation;
+      cur->next   = cur;
+      cur->prev   = cur;
+    } else {
+      start->prev      = newStation;
+      cur->next        = newStation;
+      newStation->next = start;
+      newStation->prev = cur;
+      cur              = newStation;
+    }
+
+    // Read the stations.
+    readStation(p, hash, newStation);
+    newStation->isNight    = isNight(newStation->lat, newStation->lon, now);
+    newStation->blinkState = true;
+
+    classifyDominantWeather(newStation);
+
+    p = p->next;
+  }
+
+  ok = true;
+
+cleanup:
+  if (doc) {
+    xmlFreeDoc(doc);
+  }
+
+  if (hash) {
+    xmlHashFree(hash, hashDealloc);
+  }
+
+  if (!ok) {
+    freeStations(start);
+    start = NULL;
+  }
+
+  return start;
+}
+
+/**
  * @brief   Trims a local airport ID for display.
  * @details Non-ICAO airport IDs include numbers and are three characters long,
  *          e.g. 7S3 or X01. However, AviationWeather.gov expects four-character
@@ -98,109 +400,13 @@ static bool parseUTCDateTime(const char *str, struct tm *tm) {
 }
 
 /**
- * @enum  Tag
- * @brief METAR XML tag ID.
- */
-// clang-format off
-typedef enum {
-  tagInvalid = -1,
-
-  tagResponse = 1,
-
-  tagData,
-
-  tagMETAR,
-
-  tagRawText,  tagStationId, tagObsTime,   tagLat,      tagLon, tagTemp,
-  tagDewpoint, tagWindDir,   tagWindSpeed, tagWindGust, tagVis, tagAlt,
-  tagCategory, tagWxString,  tagSkyCond,   tagVertVis,
-
-  tagSkyCover, tagCloudBase,
-
-  tagSCT,      tagFEW,       tagBKN,       tagOVC,      tagOVX, tagCLR,
-  tagSKC,      tagCAVOK,
-
-  tagVFR,      tagMVFR,      tagIFR,       tagLIFR,
-
-  tagFirst = tagResponse,
-  tagLast = tagLIFR
-} Tag;
-// clang-format on
-
-// clang-format off
-static const Tag tags[] = {
-  tagResponse,
-
-  tagData,
-
-  tagMETAR,
-
-  tagRawText,  tagStationId, tagObsTime,   tagLat,      tagLon, tagTemp,
-  tagDewpoint, tagWindDir,   tagWindSpeed, tagWindGust, tagVis, tagAlt,
-  tagCategory, tagWxString,  tagSkyCond,   tagVertVis,
-
-  tagSkyCover, tagCloudBase,
-
-  tagSCT,      tagFEW,       tagBKN,       tagOVC,      tagOVX, tagCLR,
-  tagSKC,      tagCAVOK,
-
-  tagVFR,      tagMVFR,      tagIFR,       tagLIFR,
-};
-// clang-format on
-
-// clang-format off
-static const char *tagNames[] = {
-  "response",
-
-  "data",
-
-  "METAR",
-
-  "raw_text",
-  "station_id",
-  "observation_time",
-  "latitude",
-  "longitude",
-  "temp_c",
-  "dewpoint_c",
-  "wind_dir_degrees",
-  "wind_speed_kt",
-  "wind_gust_kt",
-  "visibility_statute_mi",
-  "altim_in_hg",
-  "flight_category",
-  "wx_string",
-  "sky_condition",
-  "vert_vis_ft",
-
-  "sky_cover",
-  "cloud_base_ft_agl",
-
-  "SCT",
-  "FEW",
-  "BKN",
-  "OVC",
-  "OVX",
-  "CLR",
-  "SKC",
-  "CAVOK",
-
-  "VFR",
-  "MVFR",
-  "IFR",
-  "LIFR",
-
-  NULL
-};
-// clang-format on
-
-/**
  * @brief Initialize the tag map.
  * @param[in] hash Tag hash map.
  */
 static void initHash(xmlHashTablePtr hash) {
-  for (long i = 0; tagNames[i]; ++i)
-    xmlHashAddEntry(hash, (xmlChar *)tagNames[i], (void *)tags[i]);
+  for (long i = 0; gTagNames[i]; ++i) {
+    xmlHashAddEntry(hash, (xmlChar *)gTagNames[i], (void *)gTags[i]);
+  }
 }
 
 /**
@@ -226,14 +432,6 @@ static Tag getTag(xmlHashTablePtr hash, const xmlChar *tag) {
 
   return (Tag)p;
 }
-
-/**
- * @struct METARCallbackData
- * @brief  User data structure for parsing METAR XML.
- */
-typedef struct {
-  xmlParserCtxtPtr ctxt;
-} METARCallbackData;
 
 /**
  * @brief cURL data callback for the METAR XAML API.
@@ -457,12 +655,6 @@ static void readStation(xmlNodePtr node, xmlHashTablePtr hash, WxStation *statio
 }
 
 /**
- * @enum Intensity
- * @brief Weather intensity value.
- */
-typedef enum { intensityInvalid, intensityLight, intensityModerate, intensityHeavy } Intensity;
-
-/**
  * @brief   Classifies the dominant weather phenomenon.
  * @details Examines all of the reported weather phenomena and returns the
  *          dominant, i.e. most impactful, phenomenon.
@@ -658,170 +850,4 @@ static xmlNodePtr getChildTag(xmlNodePtr children, Tag tag, xmlHashTablePtr hash
   }
 
   return NULL;
-}
-
-WxStation *queryWx(const char *stations, int *err) {
-  CURL             *curlLib;
-  CURLcode          res;
-  char              url[4096];
-  METARCallbackData data;
-  xmlDocPtr         doc = NULL;
-  xmlNodePtr        p;
-  xmlHashTablePtr   hash = NULL;
-  Tag               tag;
-  WxStation        *start = NULL, *cur, *newStation;
-  int               count, len;
-  bool              ok = false;
-
-  *err = 0;
-
-  // Build the query string to look for reports within the last hour and a half.
-  // It is possible some stations lag more than an hour, but typically not more
-  // than an hour and a half.
-  count = strncpy_safe(url,
-                       "https://aviationweather.gov/adds/dataserver_current/httpparam?"
-                       "dataSource=metars&"
-                       "requestType=retrieve&"
-                       "format=xml&"
-                       "hoursBeforeNow=1.5&"
-                       "mostRecentForEachStation=true&"
-                       "stationString=",
-                       COUNTOF(url));
-
-  assertLog(count < COUNTOF(url), "Base URL is too large.");
-  count = COUNTOF(url) - strlen(url);
-  len   = strlen(stations);
-
-  // TODO: Eventually this should split the station string into multiple queries
-  // if it is too long. For now, just abort.
-  if (len >= count) {
-    return NULL;
-  }
-
-  // NOLINTNEXTLINE -- strncat is sufficient; sizes checked above.
-  strncat(url, stations, count);
-
-  curlLib = curl_easy_init();
-
-  if (!curlLib) {
-    return NULL;
-  }
-
-  data.ctxt = NULL;
-  curl_easy_setopt(curlLib, CURLOPT_URL, url);
-  curl_easy_setopt(curlLib, CURLOPT_WRITEFUNCTION, metarCallback);
-  curl_easy_setopt(curlLib, CURLOPT_WRITEDATA, &data);
-  res = curl_easy_perform(curlLib);
-  curl_easy_cleanup(curlLib);
-
-  if (res != CURLE_OK || !data.ctxt) {
-    *err = res;
-    return NULL;
-  }
-
-  xmlParseChunk(data.ctxt, NULL, 0, 1);
-  doc = data.ctxt->myDoc;
-  xmlFreeParserCtxt(data.ctxt);
-
-  hash = xmlHashCreate(tagLast);
-  initHash(hash);
-
-  // Find the response tag.
-  p = getChildTag(doc->children, tagResponse, hash);
-
-  if (!p) {
-    goto cleanup;
-  }
-
-  // Find the data tag.
-  p = getChildTag(p->children, tagData, hash);
-
-  if (!p) {
-    goto cleanup;
-  }
-
-  // Scan for METAR groups.
-  p = p->children;
-
-  while (p) {
-    tag = getTag(hash, p->name);
-
-    if (tag != tagMETAR) {
-      p = p->next;
-      continue;
-    }
-
-    newStation = (WxStation *)malloc(sizeof(WxStation));
-    memset(newStation, 0, sizeof(WxStation)); // NOLINT -- Size known.
-
-    // Add the station to the circular list.
-    if (!start) {
-      start = cur = newStation;
-      cur->next   = cur;
-      cur->prev   = cur;
-    } else {
-      start->prev      = newStation;
-      cur->next        = newStation;
-      newStation->next = start;
-      newStation->prev = cur;
-      cur              = newStation;
-    }
-
-    // Read the station and get the night status for classifying weather.
-    readStation(p, hash, newStation);
-    newStation->isNight    = isNight(newStation->lat, newStation->lon, newStation->obsTime);
-    newStation->blinkState = true;
-
-    classifyDominantWeather(newStation);
-
-    p = p->next;
-  }
-
-  ok = true;
-
-cleanup:
-  if (doc) {
-    xmlFreeDoc(doc);
-  }
-
-  if (hash) {
-    xmlHashFree(hash, hashDealloc);
-  }
-
-  if (!ok) {
-    freeStations(start);
-    start = NULL;
-  }
-
-  return start;
-}
-
-void freeStations(WxStation *stations) {
-  WxStation    *p;
-  SkyCondition *s;
-
-  if (!stations) {
-    return;
-  }
-
-  // Break the circular list.
-  stations->prev->next = NULL;
-
-  while (stations) {
-    p = stations;
-
-    stations = stations->next;
-
-    free(p->id);
-    free(p->localId);
-    free(p->raw);
-
-    while (p->layers) {
-      s         = p->layers;
-      p->layers = p->layers->next;
-      free(s);
-    }
-
-    free(p);
-  }
 }
