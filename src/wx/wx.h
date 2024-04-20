5122227f9a63018d98ce6453bc66af4e9115ab35
Randy Widell <randy@slakr.org>
2024-04-19 21:24:52 -0700

    Changes geo sorting to be the default sort.

--- src/wx/wx.h
+++ src/wx/wx.h
@@ -12,7 +12,7 @@
  * @enum  SortType
  * @brief Weather station sort type.
  */
-typedef enum { sortNone, sortAlpha, sortPosition } SortType;
+typedef enum { sortNone, sortPosition, sortAlpha } SortType;
 _Static_assert(sortNone == 0, "No sorting must be zero.");
 
 /**
