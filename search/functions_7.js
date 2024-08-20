8d202d7322200086f3b4811554cfd6f916b8d971
Randy Widell <randy@slakr.org>
2024-08-19 19:51:09 -0700

    R55 documentation.

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,11 +1,13 @@
 var searchData=
 [
-  ['led_5fsetcolors_0',['led_setColors',['../led_8c.html#a5f905f08cd4a8e276ee3b8b1505de4c6',1,'led_setColors(int dataPin, int dmaChannel, const LEDColor *colors, size_t count):&#160;led.c'],['../led_8h.html#a5f905f08cd4a8e276ee3b8b1505de4c6',1,'led_setColors(int dataPin, int dmaChannel, const LEDColor *colors, size_t count):&#160;led.c']]],
-  ['loadfont_1',['loadFont',['../gfx_8c.html#a4027f354f4648e592f9a0a08d4d83e5b',1,'gfx.c']]],
-  ['loadfonts_2',['loadFonts',['../gfx_8c.html#a98eb41d485f13fb80b462cc93b8fb26d',1,'gfx.c']]],
-  ['loadglobetexture_3',['loadGlobeTexture',['../globe_8c.html#ab1f414ec20f10e8ca14b287b32af2b5f',1,'globe.c']]],
-  ['loadglobetextures_4',['loadGlobeTextures',['../globe_8c.html#a5af9db4a859f547d93acce7e2a7aedc9',1,'globe.c']]],
-  ['loadicon_5',['loadIcon',['../gfx_8c.html#a370997ac250a6f90137fb677d1e6d1c5',1,'gfx.c']]],
-  ['loadicons_6',['loadIcons',['../gfx_8c.html#af0ee3b95463c812909f7933782f766a6',1,'gfx.c']]],
-  ['loadpng_7',['loadPng',['../img_8c.html#a04f8db52defeb91c926958e4577ebbcf',1,'loadPng(Png *png, const char *path):&#160;img.c'],['../img_8h.html#a04f8db52defeb91c926958e4577ebbcf',1,'loadPng(Png *png, const char *path):&#160;img.c']]]
+  ['led_5ffinalize_0',['led_finalize',['../led_8c.html#ad6923d567b5be5440c8962807a2e3997',1,'led_finalize():&#160;led.c'],['../led_8h.html#ad6923d567b5be5440c8962807a2e3997',1,'led_finalize():&#160;led.c']]],
+  ['led_5finit_1',['led_init',['../led_8c.html#a38e92a397e085db03ec3a633cf52b47c',1,'led_init(int dataPin, int dmaChannel, size_t maxLeds):&#160;led.c'],['../led_8h.html#a38e92a397e085db03ec3a633cf52b47c',1,'led_init(int dataPin, int dmaChannel, size_t maxLeds):&#160;led.c']]],
+  ['led_5fsetcolors_2',['led_setColors',['../led_8c.html#ac4683745d7a2fecc07934c89b3c23bc1',1,'led_setColors(const LEDColor *colors, size_t count):&#160;led.c'],['../led_8h.html#ac4683745d7a2fecc07934c89b3c23bc1',1,'led_setColors(const LEDColor *colors, size_t count):&#160;led.c']]],
+  ['loadfont_3',['loadFont',['../gfx_8c.html#a4027f354f4648e592f9a0a08d4d83e5b',1,'gfx.c']]],
+  ['loadfonts_4',['loadFonts',['../gfx_8c.html#a98eb41d485f13fb80b462cc93b8fb26d',1,'gfx.c']]],
+  ['loadglobetexture_5',['loadGlobeTexture',['../globe_8c.html#ab1f414ec20f10e8ca14b287b32af2b5f',1,'globe.c']]],
+  ['loadglobetextures_6',['loadGlobeTextures',['../globe_8c.html#a5af9db4a859f547d93acce7e2a7aedc9',1,'globe.c']]],
+  ['loadicon_7',['loadIcon',['../gfx_8c.html#a370997ac250a6f90137fb677d1e6d1c5',1,'gfx.c']]],
+  ['loadicons_8',['loadIcons',['../gfx_8c.html#af0ee3b95463c812909f7933782f766a6',1,'gfx.c']]],
+  ['loadpng_9',['loadPng',['../img_8c.html#a04f8db52defeb91c926958e4577ebbcf',1,'loadPng(Png *png, const char *path):&#160;img.c'],['../img_8h.html#a04f8db52defeb91c926958e4577ebbcf',1,'loadPng(Png *png, const char *path):&#160;img.c']]]
 ];
